c88fad1183315c180b5eb12f661b6ffb2a316870
william <williamrochajr@gmail.com>
2021-05-27 20:49:26 -0300

    Método PUT e PATCH

--- routes/accounts.js
+++ routes/accounts.js
@@ -53,6 +53,35 @@ router.delete("/:id", async (req, res) => {
     } catch (error) {
         
     }
+})  
+
+router.put("/", async (req, res) => {
+    try {
+        const account = req.body;
+
+        const data = JSON.parse(await readFile(global.fileName))
+        const index = data.accounts.findIndex(a => a.id === account.id)
+        data.accounts[index] = account
+        await writeFile(global.fileName, JSON.stringify(data))
+        res.send(account)
+    } catch (err) {
+        res.status(400).send({error: err.message}); 
+    }
+})
+
+router.patch("/updateBalance", async (req, res) => {
+    try {
+        const account = req.body;
+
+        const data = JSON.parse(await readFile(global.fileName))
+        const index = data.accounts.findIndex(a => a.id === account.id)
+
+        data.accounts[index].balance = account.balance
+        await writeFile(global.fileName, JSON.stringify(data))
+        res.send(data.accounts[index])
+    } catch (err) {
+        res.status(400).send({error: err.message}); 
+    }
 })
 
 export default router
