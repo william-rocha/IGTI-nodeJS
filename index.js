8e06e56437a48af2e12347610cf0e96f8eaa791c
william-rocha <williamrochajr@gmail.com>
2021-05-30 17:19:44 -0300

    add cors

--- index.js
+++ index.js
@@ -2,44 +2,48 @@ import express from "express"
 import accountRouter from "./routes/accounts.js"
 import { promises as fs } from "fs"
 import winston from "winston"
+import cors from "cors"
+
 const { readFile, writeFile } = fs
 
 // var global do node para evitar ficar reescrevendo
 global.fileName = "accounts.json"
 
-const {combine, timestamp, label, printf} =winston.format
-const myFormat = printf(({ level, message, label, timestamp}) => {
+const { combine, timestamp, label, printf } = winston.format
+const myFormat = printf(({ level, message, label, timestamp }) => {
     return `${timestamp} [${label}] ${level}: ${message}`
 })
 global.logger = winston.createLogger({
     level: "silly",
     transports: [
         new (winston.transports.Console)(),
-        new (winston.transports.File)({filename: "my-banck-api.log"})
+        new (winston.transports.File)({ filename: "my-banck-api.log" })
     ],
     format: combine(
-        label({ label: "my-bank-api"}),
+        label({ label: "my-bank-api" }),
         timestamp(),
         myFormat
     )
 })
 const app = express()
 app.use(express.json())
-
+// desta forma libera todos os endpoints
+app.use(cors())
+app.use(express.static("public"))
 app.use("/account", accountRouter)
 
 app.listen(3000, async () => {
 
     try {
-        await readFile("accounts.json", )
+        await readFile("accounts.json",)
         logger.info("Api started!");
-    } catch(err) {
+    } catch (err) {
         const initialJson = {
-            nextId: 1, 
+            nextId: 1,
             accounts: []
         }
         writeFile(global.fileName, JSON.stringify(initialJson)).then(() => {
-           logger.info("Api started and file created!");
+            logger.info("Api started and file created!");
         }).catch(err => {
             logger.error(err);
         })
--- routes/accounts.js
+++ routes/accounts.js
@@ -1,12 +1,10 @@
 import express from "express";
 import { promises as fs } from "fs";
 
-
-const {readFile, writeFile} = fs
+const { readFile, writeFile } = fs
 const router = express.Router()
 
 router.post("/", async (req, res, next) => {
-    // console.log(req.body);
     try {
 
         let account = req.body;
@@ -15,21 +13,18 @@ router.post("/", async (req, res, next) => {
             throw new Error("Name e Balance são obrigatórios")
         }
         const data = JSON.parse(await readFile(global.fileName));
-        // account = {id: data.nextId++, ...account}
-        // VALIDAÇÃO vamos inserir induvidualmente para evitar add campos indevidos
-        account = {id: data.nextId++, name: account.name, balance: account.balance }
+        account = { id: data.nextId++, name: account.name, balance: account.balance }
         data.accounts.push(account);
-        // data, null, 2: para manter o json formatado
-        // mas para o arquivo ficar menor o ideal e mante-lo não formatado
         await writeFile(global.fileName, JSON.stringify(data, null, 2))
         // res.end();
         res.send(account)
         logger.info(`POST /account - ${JSON.stringify(account)}`)
-    } catch(err) {
+    } catch (err) {
         next(err)
     }
 })
-
+// podemos liberar o cors individualmente
+// router.get("/", cors(), async (req, res, next) => {
 router.get("/", async (req, res, next) => {
     try {
         // aqui sempre de ser let porque estamos atribuindo outro obj
@@ -58,12 +53,12 @@ router.delete("/:id", async (req, res, next) => {
         let data = JSON.parse(await readFile(global.fileName))
         data.accounts = data.accounts.filter(account => account.id !== parseInt(req.params.id))
         await writeFile(global.fileName, JSON.stringify(data, null, 2))
-        res.end()    
+        res.end()
         logger.info(`DELETE /account/:id - ${req.params.id}`)
     } catch (error) {
         next(err)
     }
-})  
+})
 
 router.put("/", async (req, res, next) => {
     try {
@@ -91,10 +86,10 @@ router.put("/", async (req, res, next) => {
 router.patch("/updateBalance", async (req, res, next) => {
     try {
         const account = req.body;
-        
+
         const data = JSON.parse(await readFile(global.fileName))
         const index = data.accounts.findIndex(a => a.id === account.id)
-        console.log('data',index);
+        console.log('data', index);
         if (!account.id || account.balance == null) {
             throw new Error("Id e Balance são obrigatórios")
         }
@@ -110,9 +105,9 @@ router.patch("/updateBalance", async (req, res, next) => {
     }
 })
 
-router.use((err,req, res, next) => {
+router.use((err, req, res, next) => {
     logger.error(`${req.method} ${req.baseUrl} ${err.message}`);
-    res.status(400).send({error: err.message})
-} )
+    res.status(400).send({ error: err.message })
+})
 
 export default router
